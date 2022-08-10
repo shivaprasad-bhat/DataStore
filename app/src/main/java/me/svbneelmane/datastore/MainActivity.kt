cb2ec0a9dd2763134d388b3613224a79fe0ee3da
shivaprasad-bhat <shivaprasad@svbneelmane.me>
2022-08-10 18:08:55 +0530

    dev(): Final implementation

--- app/src/main/java/me/svbneelmane/datastore/MainActivity.kt
+++ app/src/main/java/me/svbneelmane/datastore/MainActivity.kt
@@ -1,7 +1,16 @@
 package me.svbneelmane.datastore
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import me.svbneelmane.datastore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +19,45 @@ class MainActivity : AppCompatActivity() {
     private val binding
         get() = _binding!!
 
+    // Data store object - Taken from context
+    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSave.setOnClickListener {
+            lifecycleScope.launch {
+                save(
+                    binding.etKey.text.toString(),
+                    binding.etValue.text.toString()
+                )
+            }
+        }
+
+        binding.btnRead.setOnClickListener {
+            lifecycleScope.launch {
+                val value = read(
+                    binding.etRetriveKey.text.toString()
+                )
+                binding.tvReadValue.text = value ?: "No key found in the data store"
+            }
+        }
+    }
+
+    private suspend fun save(key: String, value: String) {
+        val dataStoreKey = stringPreferencesKey(key)
+        applicationContext.dataStore.edit { settings ->
+            settings[dataStoreKey] = value
+        }
+
+    }
+
+    private suspend fun read(key: String): String? {
+        val dataStoreKey = stringPreferencesKey(key)
+        val preferences = applicationContext.dataStore.data.first()
+        return preferences[dataStoreKey]
     }
 
     override fun onDestroy() {
